5930c8c0a3e71e9473e87c1448e0246384a71d90
webdevelopement01 <mdabusayam41@gmail.com>
2025-06-06 01:15:07 +0600

    if else code add

--- if_else.cpp
+++ if_else.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
 int main (){
-    
+    int x = 13;
+    if (x%3 == 0){
+        cout << "Event\n";
+    }
+    else{
+        cout << "Odd\n";
+
+    }
+    return 0;
 }
